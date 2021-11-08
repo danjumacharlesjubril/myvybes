0ffaee7bf89c6a5d52dd56b5272ecb7b1a780c05
Danjuma Jubril Charles <charlesjubril@yahoo.com>
2021-11-08 16:06:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Welcome to your App!")
+})
+
+app.listen(PORT, function () {
+  console.log(`Express server listening on port ${PORT}`)
+})
